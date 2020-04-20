dcde0bf96f6d4db75473a212cd090ab9ddf46b97
likunyao <nwpulky@gmail.com>
2020-04-20 15:36:35 +0800

    add table sort

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,10 +3,12 @@
     :data="records"
     stripe
     style="width: 100%"
+    :default-sort="{prop: 'date', order: 'ascending'}"
   >
     <el-table-column
       prop="date"
       label="日期"
+      sortable
       width="180"
     >
     </el-table-column>
